491942da9357b46c3a3cdb45375450b0d45c15b9
Sebastian Riley Rasor <https://www.sebastianrasor.com/contact>
2021-10-01 03:28:56 -0400

    use cc0 instead of unlicense

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Shaker - A Discord bot which validates ownership of Handshake names
+// Written in 2021 by Sebastian Riley Rasor
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to the
+// public domain worldwide. This software is distributed without any
+// warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software. If not, see
+// <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 const { Client, SQLiteProvider } = require('discord.js-commando');
 const dns = require('dns');
 const fetch = require('node-fetch');
